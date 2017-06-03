3b12a7eed97b64cb0d98dc168c6669d6dd9d1f68
jp <jp@climberhotel.com>
2017-06-03 10:45:10 +0100

    invite friends call share with system app

--- SwipeAMovie/www/js/controllers.js
+++ SwipeAMovie/www/js/controllers.js
@@ -1,16 +1,19 @@
 angular.module('app.controllers', [])
-  
-.controller('createAnEventCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+
+.controller('createAnEventCtrl', ['$scope', '$stateParams', '$cordovaSocialSharing', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope, $stateParams, $cordovaSocialSharing) {
     var vm = this;
 
     vm.entity = { date: new Date(), time: new Date(1970, 0, 0, 12, 34, 0, 0) };
 
+    $scope.test=function(){
+      $cordovaSocialSharing.share('This is my message', 'Subject string', null, 'http://www.mylink.com');
+    }
 
 }])
-   
+
 .controller('invitationReceivedCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -18,7 +21,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('votingMoviesCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -26,7 +29,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('votingMoviesDetailsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -34,7 +37,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('votingGenresCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -42,7 +45,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('resultsCountdownUserCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -50,7 +53,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('resultsCountdownAdminCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -58,7 +61,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('resultsDecisionCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -66,4 +69,3 @@ function ($scope, $stateParams) {
 
 
 }])
- 
